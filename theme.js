3091e874d104265d23b169ee674b8f3356acd019
chitacan <chitacan@gmail.com>
2019-10-15 18:47:37 +0900

    init.

---
+++ theme.js
@@ -0,0 +1,29 @@
+export default {
+  googleFont: 'https://fonts.googleapis.com/css?family=Noto+Serif+KR&display=swap',
+  styles: {
+    root: {
+      wordBreak: 'keep-all'
+    },
+    p: {
+      ml: '2em',
+      mr: '2em'
+    },
+    h2: {
+      textAlign: 'center'
+    },
+    ul: {
+      li: {
+        mt: '10px',
+        ml: '2em',
+        mr: '2em',
+        ul: {
+          li: {
+            fontSize: '.7em',
+            ml: '0',
+            mr: '0'
+          }
+        }
+      },
+    }
+  }
+}
